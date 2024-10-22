4e70e7460274dbf9c95349034a68603ba943deb8
opinti <92609579+opinti@users.noreply.github.com>
2024-10-22 09:40:41 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     packages=find_packages(include=["specmf"]),
     version="0.1.0",
     description="Library to perform spectral multifidelity modeling",
-    author="Orazio Pinti, orazio.pinti@gmail.com",
+    author="Orazio Pinti",
+    author_email="orazio.pinti@gmail.com",
+    url="https://github.com/opinti/spectral-multifidelity",
     python_requires=">=3.12",
     install_requires=[
         "numpy==1.26.0",
@@ -16,4 +18,11 @@ setup(
         "scikit-optimize==0.8.1",
         "umap-learn>=0.5.6",
     ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "black",
+            "flake8",
+        ],
+    },
 )
