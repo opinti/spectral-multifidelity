086cfb2c21e27a565372d3ebb8557fd8c838469d
opinti <92609579+opinti@users.noreply.github.com>
2025-11-27 11:47:33 -0800

    move synth data utils

---
+++ data/synthetic/__init__.py
@@ -0,0 +1,22 @@
+from .synthetic import (
+    affine_transform,
+    bend_half_moon,
+    generate_circles_2d,
+    generate_lf_hf_example,
+    make_half_moon_cluster,
+    make_s_cluster,
+    make_torus_cluster,
+    unfold_s_curve,
+)
+
+
+__all__ = [
+    "affine_transform",
+    "bend_half_moon",
+    "generate_circles_2d",
+    "generate_lf_hf_example",
+    "make_half_moon_cluster",
+    "make_s_cluster",
+    "make_torus_cluster",
+    "unfold_s_curve",
+]
